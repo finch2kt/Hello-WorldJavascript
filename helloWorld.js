ecefa4031a7fedc4db3d291763ae14abfa1c29f6
finch2kt <finch2kt@Dukes.jmu.edu>
2017-02-24 11:57:55 -0500

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,13 +1,13 @@
 //prints Hello World to console
 console.log("Hello World");
 
-// print 'Hello World' to new HTTP connections.
-var http = require('http');
+// // print 'Hello World' to new HTTP connections.
+// var http = require('http');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end('Hello World\n');
-}).listen(8080);
-//go to 'http://localhost:8080' you should see 'Hello World' message.
+// http.createServer(function (request, response) {
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+//     response.end('Hello World\n');
+// }).listen(8080);
+// //go to 'http://localhost:8080' you should see 'Hello World' message.
 
-console.log('Server started');
+// console.log('Server started');
